a8f1816fa47388f46a467c20eb242eebd8aaf781
Ryan Pitts <ryan.a.pitts@gmail.com>
2014-09-05 16:49:17 -0700

    add test command view

--- membot/apps/membot/views.py
+++ membot/apps/membot/views.py
@@ -1,4 +1,24 @@
-from django.http import HttpResponse
+import os
+
+from django.http import HttpResponse, HttpResponseForbidden, JsonResponse
+from django.views.generic import View
+
+SLACK_TOKEN = os.environ['SLACK_TOKEN']
+SLACK_TEAM_ID = os.environ['SLACK_TEAM_ID']
 
 def homepage(request):
     return HttpResponse('Hello world this is membot')    
+
+class CommandView(View):
+    def post(self, request, *args, **kwargs):
+        received = request.POST
+        
+        # make sure command is coming from the right place
+        if received.token != SLACK_TOKEN or received.team_id != SLACK_TEAM_ID:
+            return HttpResponseForbidden
+
+        response = {
+            'text': 'Membot is here, <@%s>! I heard you say \'%s\'' % (received.user_name, received.text)
+        }
+
+        return JsonResponse(response)
