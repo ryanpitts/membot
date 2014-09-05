89e0bd4334289cd19729aff0b7934737e1ede264
Ryan Pitts <ryan.a.pitts@gmail.com>
2014-09-05 16:55:08 -0700

    change some path things for Heroku

--- membot/apps/membot/views.py
+++ membot/apps/membot/views.py
@@ -4,7 +4,6 @@ from django.http import HttpResponse, HttpResponseForbidden, JsonResponse
 from django.views.generic import View
 
 SLACK_TOKEN = os.environ['SLACK_TOKEN']
-SLACK_TEAM_ID = os.environ['SLACK_TEAM_ID']
 
 def homepage(request):
     return HttpResponse('Hello world this is membot')    
@@ -12,10 +11,13 @@ def homepage(request):
 class CommandView(View):
     def post(self, request, *args, **kwargs):
         received = request.POST
+        print received
         
         # make sure command is coming from the right place
-        if received.token != SLACK_TOKEN or received.team_id != SLACK_TEAM_ID:
+        if received.token != SLACK_TOKEN:
             return HttpResponseForbidden
+        else:
+            print received.token
 
         response = {
             'text': 'Membot is here, <@%s>! I heard you say \'%s\'' % (received.user_name, received.text)
