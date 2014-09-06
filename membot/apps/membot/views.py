806aa6cbbdd17e6bc05768e6b24a18804bf37fce
Ryan Pitts <ryan.a.pitts@gmail.com>
2014-09-05 17:12:03 -0700

    @csrf_exempt on command view

--- membot/apps/membot/views.py
+++ membot/apps/membot/views.py
@@ -1,6 +1,7 @@
 import os
 
 from django.http import HttpResponse, HttpResponseForbidden, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
 
 SLACK_TOKEN = os.environ['SLACK_TOKEN']
@@ -8,6 +9,7 @@ SLACK_TOKEN = os.environ['SLACK_TOKEN']
 def homepage(request):
     return HttpResponse('Hello world this is membot')    
 
+@csrf_exempt
 class CommandView(View):
     def post(self, request, *args, **kwargs):
         received = request.POST
