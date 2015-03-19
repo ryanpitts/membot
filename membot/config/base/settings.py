1aa21d2a708312bedf260ba4775af9235459e195
Ryan Pitts <ryan.a.pitts@gmail.com>
2015-03-18 17:04:15 -0700

    disable projects app

--- membot/config/base/settings.py
+++ membot/config/base/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'membot.apps.membot',
-    'membot.apps.projects',
+    #'membot.apps.projects',
     'corsheaders',
 )
 
--- membot/config/base/urls.py
+++ membot/config/base/urls.py
@@ -2,15 +2,15 @@ from django.conf import settings
 from django.conf.urls import url, patterns, include
 from django.contrib import admin
 
-from membot.apps.projects.views import ProjectListView
+#from membot.apps.projects.views import ProjectListView
 
 urlpatterns = patterns('',
-    url(
-        regex   = '^$',
-        view    = ProjectListView.as_view(),
-        kwargs  = {},
-        name    = 'homepage',
-    ),
+    #url(
+    #    regex   = '^$',
+    #    view    = ProjectListView.as_view(),
+    #    kwargs  = {},
+    #    name    = 'homepage',
+    #),
     url(r'^membot/', include('membot.apps.membot.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
